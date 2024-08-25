42fbef80cb435787bf0dd224abc5ccaa21c010e2
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-08-25 15:13:36 +0700

    encrypt my secret key and generate a new one with sample environment

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_@r7mrlxvx%=3vtl_q=4kh(x0hb4kpqb%vtwmcd)a7^c+@1glg"
+
+
+SECRET_KEY = config('SECRET_KEY', default='fake-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
